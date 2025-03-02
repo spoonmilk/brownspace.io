f41a42fed2a29717be0db425e3919c5225df07c7
spoonmilk <haku31229@gmail.com>
2025-03-02 17:51:59 -0500

    fix: added operations and fixed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,35 +1,15 @@
-import Logo from "@/components/logo";
-
 export const Footer = () => {
   return (
     <footer id="footer">
       <hr className="w-11/12 mx-auto" />
 
       <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
-        <div className="col-span-full xl:col-span-2">
-          <a
-            rel="noreferrer noopener"
-            href="/"
-            className="font-bold text-xl flex place-items-center hidden xl:flex"
-          >
-            <Logo />
-          </a>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Github
-            </a>
-          </div>
-
-
-        </div>
-
+        <img src="icon.png" alt="Brown Space Engineering Logo" className="w-15 dark:invert" />
+        <img src="brownengn.png" alt="Brown School of Engineering Logo" className="w-15 dark:hidden" />
+        <img src="brownengn_whtxt.png" alt="Brown School of Engineering Logo" className="w-15 hidden dark:block" />
+        <a href="https://github.com/BrownSpaceEngineering" target="_blank" rel="noopener noreferrer">
+          <img src="/github-mark.svg" alt="GitHub Logo" className="w-20 dark:invert" />
+        </a>
      </section>
     </footer>
   );
