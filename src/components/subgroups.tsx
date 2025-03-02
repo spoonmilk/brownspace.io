2c851120b7da3c31fdfe589943c0b944bd9b95cf
spoonmilk <haku31229@gmail.com>
2025-03-02 15:48:01 -0500

    fix: subgroup light mode

--- src/components/subgroups.tsx
+++ src/components/subgroups.tsx
@@ -9,11 +9,11 @@ const Subgroup = ({ subgroupName, description, imageUrl }: { subgroupName: strin
   const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0.5, 1, 0.5]);
 
   return (
-      <motion.div ref={ref} style={{ y, scale, opacity }} className="flex justify-center items-center bg-zinc-900 p-4 rounded-lg shadow-lg mb-20 w-1/2 mx-auto">
+      <motion.div ref={ref} style={{ y, scale, opacity }} className="flex justify-center items-center dark:bg-zinc-900 p-4 rounded-lg shadow-lg mb-20 w-1/2 mx-auto">
           <div className="text-center">
               <img src={imageUrl} alt={subgroupName} width={500} height={200} className="mx-auto rounded-lg "/>
-              <h2 className="mt-4 text-xl font-semibold text-white pb-2">{subgroupName}</h2>
-              <p className="text-white">{description}</p>
+              <h2 className="mt-4 text-xl font-semibold dark:text-white pb-2">{subgroupName}</h2>
+              <p className="dark:text-white">{description}</p>
           </div>
       </motion.div>
   );
@@ -30,8 +30,8 @@ export const Subgroups = () => {
                   { team: 'Avionics', description: "Avionics is responsible for all the electronic hardware aboard the CubeSat. This involves developing all aspects of the satellite's power system, from the solar panels, and batteries, all the way to distribution and sensing.", imageUrl: '/subgroups/avionics_photo.png' },
                   { team: 'Flight Software', description: 'Tasked with hardware-software integration, merging PVDX’s complex array of sensors and actuators into a central operating system that manages the transfer of information between the satellite and the ground station.', imageUrl: '/subgroups/fsw-photo.JPEG' },
                   { team: 'Ground Software', description: 'Tasked with producing a web-app to allow student interaction with PVDX, managing our own website, and building a ground station to communicate with PVDx once in orbit.', imageUrl: '/subgroups/gsw-photo.jpg' },
-                  { team: 'Operations', description: 'Handles fundraising, sponsorships, orders, and events for the organization.' },
-                  { team: 'R&D', description: "Focuses on unique, space-related research projects outside the scope of BSE's bigger projects. Last year, R&D competed in the Plant the Moon Challenge, experimenting with cacti growth in lunar highlight and mare simulant, and winning the award for Best Analysis of Data." },
+                  { team: 'Operations', description: 'Handles fundraising, sponsorships, orders, and events for the organization.', imageUrl: 'logo.png' },
+                  { team: 'R&D', description: "Focuses on unique, space-related research projects outside the scope of BSE's bigger projects. Last year, R&D competed in the Plant the Moon Challenge, experimenting with cacti growth in lunar highlight and mare simulant, and winning the award for Best Analysis of Data.", imageUrl: 'logo.png' },
                   { team: 'Structures', description: 'Designs, manufactures and tests a majority of the components of the physical satellite. There are many independent projects operating within the structures subgroup: chassis, arm mechanism, internal structures, external structures and testing.', imageUrl: '/subgroups/structures-photo.jpg' },
               ].map((group, index) => (
                   <Subgroup key={index} subgroupName={group.team} description={group.description} imageUrl={group.imageUrl}/>
